b7413d011eac860c9b22363e2d9b887122bbeb9c
slux83 <slux83@70b9100b-7944-0410-84ee-ffaaac3ce8d5>
2008-03-25 17:38:33 +0000

    huge refactoring in agree to 'Layers'

---
+++ src/gui/handles/VmController.h
@@ -0,0 +1,50 @@
+/**
+ * VisualNetkit is an advanced graphical tool for NetKit <http://www.netkit.org>
+ * Copyright (C) 2008  Alessio Di Fazio, Paolo Minasi
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef VMCONTROLLER_H_
+#define VMCONTROLLER_H_
+
+#include <QLinkedList>
+#include <QPair>
+
+#include "../VirtualMachineItem.h"
+#include "../../core/VirtualMachine.h"
+#include "../AddVmForm.h"
+
+/**
+ * This class is the singleton controller for the virtual machines.
+ * It's also map the graphical (view) item with the domain object (model)
+ */
+class VmController
+{
+	
+private:
+	static VmController *instance;
+	AddVmForm *addVm;
+	
+	/* the pair: <VIEW, DOMAIN> */
+	QLinkedList< QPair<VirtualMachineItem*, VirtualMachine*> > mappings;
+	
+public:
+	VmController();
+	virtual ~VmController();
+	static VmController* getInstance();
+	void addVirtualMachine();
+};
+
+#endif /*VMCONTROLLER_H_*/
