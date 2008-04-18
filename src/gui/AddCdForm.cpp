9c60814051b87384292a763d073c3102bebfbe1f
slux83 <slux83@70b9100b-7944-0410-84ee-ffaaac3ce8d5>
2008-04-18 10:05:35 +0000

    Added the form (dialog) for insert a collision domain (not finished, but works)

---
+++ src/gui/AddCdForm.cpp
@@ -0,0 +1,58 @@
+/**
+ * VisualNetkit is an advanced graphical tool for NetKit <http://www.netkit.org>
+ * Copyright (C) 2008  Alessio Di Fazio, Paolo Minasi
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "AddCdForm.h"
+#include <QDesktopWidget>
+
+/**
+ * Constructor
+ */
+AddCdForm::AddCdForm(QWidget *parent) : QDialog(parent)
+{
+	/* Connect the UI resource to this QWidget */
+	setupUi(this);
+	
+	/* Centering the gui */
+	QDesktopWidget dw;
+	QRect geometry = dw.screenGeometry();
+	setGeometry((geometry.width() / 2) - (width() / 2),		//center X
+				(geometry.height() / 2) - (height() / 2),	//center Y
+				width(),
+				height());
+	
+	/* Connects */
+	connect(cdFormConfirmBox, SIGNAL(accepted()),
+			this, SLOT(handleUserConfirm()));
+}
+
+/**
+ * Deconstructor
+ */
+AddCdForm::~AddCdForm()
+{
+}
+
+/**
+ * [PRIVATE-SLOT]
+ * Handle the user form confirm
+ * pre-operations on the input
+ */
+void AddCdForm::handleUserConfirm()
+{
+	
+}
---
+++ src/gui/handles/CdMapper.h
@@ -0,0 +1,45 @@
+/**
+ * VisualNetkit is an advanced graphical tool for NetKit <http://www.netkit.org>
+ * Copyright (C) 2008  Alessio Di Fazio, Paolo Minasi
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CDMAPPER_H_
+#define CDMAPPER_H_
+
+#include <QObject>
+#include <QPointF>
+#include "../AddCdForm.h"
+
+/**
+ * This class is the singleton controller for the collision domain
+ * It's also map the graphical (view) item with the domain object (model)
+ */
+class CdMapper : public QObject
+{
+	Q_OBJECT
+	
+private:
+	static CdMapper *instance;
+	AddCdForm *addCdForm;
+
+public:
+	CdMapper();
+	virtual ~CdMapper();
+	static CdMapper* getInstance();
+	void showAddCdForm(QPointF pos);
+};
+
+#endif /*CDMAPPER_H_*/
