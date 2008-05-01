d339b89af06d75ac2ce08645f279fcec6cf68c6a
paolo.minasi <paolo.minasi@70b9100b-7944-0410-84ee-ffaaac3ce8d5>
2008-05-01 22:24:45 +0000

    Starts "netkit like" file persistency.

---
+++ src/persistence/LabSaver.h
@@ -0,0 +1,35 @@
+/**
+ * VisualNetkit is an advanced graphical tool for NetKit <http://www.netkit.org>
+ * Copyright (C) 2008  Alessio Di Fazio, Paolo Minasi
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LABSAVER_H_
+#define LABSAVER_H_
+
+class LabSaver
+{
+public:
+	LabSaver();
+	virtual ~LabSaver();
+	
+	bool saveLab();
+	
+private:
+	bool saveLabConf();
+	bool saveRouterConf();
+};
+
+#endif /*LABSAVER_H_*/
