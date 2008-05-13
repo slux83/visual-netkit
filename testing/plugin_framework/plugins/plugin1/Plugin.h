66d8dbc61c34940104871cbe85ed063a13ad383d
slux83 <slux83@70b9100b-7944-0410-84ee-ffaaac3ce8d5>
2008-05-13 08:28:01 +0000

    added a test for plugin framework

---
+++ testing/plugin_framework/plugins/plugin1/Plugin.h
@@ -0,0 +1,57 @@
+/**
+ * A simple plugin architecture.
+ * Copyright (C) 2008  Alessio Di Fazio
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PLUGIN_H_
+#define PLUGIN_H_
+
+#include <QObject>
+#include <QString>
+#include "../../PluginInterface.h"
+
+/**
+ * A simple plugin that do nothing
+ */
+class Plugin : public PluginInterface
+{
+	
+private:
+	QString description, name;
+	
+public:
+	Plugin();
+	virtual ~Plugin() {};
+	QString getDescription() { return QString(name + QString(", ") + description); };
+};
+
+
+/******************* PLUGIN FACTORIES ***********************/
+
+/* Factory (creator) */
+extern "C" PluginInterface* createPlugin()
+{
+    return new Plugin();
+}
+
+/* Factory (destroyer) */
+extern "C" void destroyPlugin(PluginInterface* p)
+{
+    delete p;
+}
+
+
+#endif
