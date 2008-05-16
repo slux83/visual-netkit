ba6d5249f10966819dd2fc603469837fc956682e
slux83 <slux83@70b9100b-7944-0410-84ee-ffaaac3ce8d5>
2008-05-16 10:00:49 +0000

    added some features for the plugin framework

---
+++ trunk/src/plugin_framework/PluginLoaderFactory.cpp
@@ -0,0 +1,65 @@
+/**
+ * VisualNetkit is an advanced graphical tool for NetKit <http://www.netkit.org>
+ * Copyright (C) 2008  Alessio Di Fazio, Paolo Minasi
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "PluginLoaderFactory.h"
+
+/**
+ * Constructor
+ */
+PluginLoaderFactory::PluginLoaderFactory(const QString &fileName, QObject *parent)
+	: QLibrary(fileName, parent)
+{
+	
+}
+
+/**
+ * Deconstructor
+ */
+PluginLoaderFactory::~PluginLoaderFactory()
+{
+}
+
+/**
+ * Factory function that return a new instance of the plugin proxy
+ * NOTE: the sistem registry is the caller
+ */
+PluginProxy * PluginLoaderFactory::createPlugin(QObject *baseElement)
+{
+	
+}
+
+/**
+ * Init and check the plugin consistance
+ */
+bool PluginLoaderFactory::initPluginLibrary()
+{
+	createPluginFactory = (createPlugin_t *)resolve("createPlugin");
+	destroyPluginFactory = (destroyPlugin_t *)resolve("destroyPlugin");
+	
+	if(!createPluginFactory || !destroyPluginFactory)
+	{
+		qWarning()	<< "ERROR: cannot resolve factories for plugin:"
+					<< fileName() << "\n" << loader.errorString();
+		return false;
+	}
+	
+	//init the instance, and get the QSetting
+	PluginProxy *fakeProxy = createPluginFactory();
+	fakeProxy->get
+	
+}
---
+++ trunk/src/plugin_framework/PluginLoaderFactory.h
@@ -0,0 +1,47 @@
+/**
+ * VisualNetkit is an advanced graphical tool for NetKit <http://www.netkit.org>
+ * Copyright (C) 2008  Alessio Di Fazio, Paolo Minasi
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PLUGINLOADERFACTORY_H_
+#define PLUGINLOADERFACTORY_H_
+
+#include <QLibrary>
+
+/**
+ * This clas is a loader for a single external plugin
+ */
+class PluginLoaderFactory : public QLibrary
+{
+private:
+	createPlugin_t* createPluginFactory;
+	destroyPlugin_t* destroyPluginFactory;
+	QString name, description, version, deps, author, type;
+	
+public:
+	PluginLoaderFactory(const QString &fileName, QObject *parent = 0);
+	virtual ~PluginLoaderFactory();
+	PluginProxy * createPlugin(QObject *baseElement);
+	bool initPluginLibrary();
+};
+
+/**
+ *  the types of factories
+ */
+typedef PluginProxy* createPlugin_t();
+typedef void destroyPlugin_t(PluginProxy*);
+
+#endif /*PLUGINLOADERFACTORY_H_*/
