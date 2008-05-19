1b2441231505d57ff6342b69c02631c88dfba375
paolo.minasi <paolo.minasi@70b9100b-7944-0410-84ee-ffaaac3ce8d5>
2008-05-19 21:21:18 +0000

    Compila e carica i plugin!

--- src/plugin_framework/PluginLoaderFactory.cpp
+++ src/plugin_framework/PluginLoaderFactory.cpp
@@ -70,9 +70,9 @@ bool PluginLoaderFactory::initPluginLibrary()
 	QSettings* pluginSetting = tester->getMySettings();
 	
 	/* Validate settings */
-	pluginSetting->beginGroup("General");
+	pluginSetting->beginGroup("generic");
 	QStringList generalKeys = pluginSetting->childKeys();
-	qDebug() << "keys:" << generalKeys;
+	qDebug() << "keys:" << generalKeys << "group:" << pluginSetting->group();
 	
 	//validate keys (General)
 	if(retVal && (!generalKeys.contains("name") ||
