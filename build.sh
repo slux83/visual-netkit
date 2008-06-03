1c90201339bdf6973395a5f3fe43ed9610f0e9b7
slux83 <slux83@70b9100b-7944-0410-84ee-ffaaac3ce8d5>
2008-06-03 19:34:03 +0000

    qmake fixed

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-QMAKE="qmake4"
+QMAKE="qmake"
 GDB="gdb"
 VN_PLUGINS=( "src/plugin_dev/test" "src/plugin_dev/ipv4" "src/plugin_dev/mac")
 VN_HOME=`pwd`
