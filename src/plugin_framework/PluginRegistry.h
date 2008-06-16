daed2d651a4ac59f5e9050635c2309719429db6d
slux83 <slux83@70b9100b-7944-0410-84ee-ffaaac3ce8d5>
2008-06-16 11:58:02 +0000

    Try to fix ubuntu compile (linking) error on plugins

--- src/plugin_framework/PluginRegistry.h
+++ src/plugin_framework/PluginRegistry.h
@@ -21,11 +21,17 @@
 
 #include <QMap>
 #include <QObject>
-#include "PluginLoaderFactory.h"
+
 #include "../core/VirtualMachine.h"
 #include "../core/CollisionDomain.h"
 #include "../core/HardwareInterface.h"
 
+/**
+ * IMPORTANT:
+ * DO NOT INCLUDE OTHER HEADERS HERE, BROKE PLUGINS COMPILATION
+ * Use "class XXX" instead and "#include XXX" header inside PluginRegistry.cpp
+ */
+class PluginLoaderFactory;
 class PluginProxy;
 
 /**
