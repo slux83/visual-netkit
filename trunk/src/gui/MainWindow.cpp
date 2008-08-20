3825c6e7bf3066acd0163d39deddcbbb8fdb1bcb
slux83 <slux83@70b9100b-7944-0410-84ee-ffaaac3ce8d5>
2008-08-20 16:10:02 +0000

    Property editor (main gui) works fine! :)

--- trunk/src/gui/MainWindow.cpp
+++ trunk/src/gui/MainWindow.cpp
@@ -57,6 +57,10 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 	
 	aboutDialog = NULL;
 	
+	//init actian menu
+	actionMenu = new QMenu();
+	actionsToolButton->setMenu(actionMenu);
+		
 	//populte the 'View' menu
 	populateViewMenu();
 	
@@ -74,7 +78,6 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
 	statusBar()->showMessage(tr("Ready"), 10000);
 	
 	clearPropertyDock();
-
 }
 
 /**
@@ -187,6 +190,14 @@ void MainWindow::createConnections()
 	//connect: about action
 	connect(actionAboutVisualNetkit, SIGNAL(triggered()),
 			this, SLOT(showAbout()));
+	
+	//connect: action property menu
+	connect(actionMenu, SIGNAL(triggered(QAction*)),
+			this, SLOT(handlePropertyAction(QAction*)));
+	
+	//connect: prop tree view click item
+	connect(propertiesTreeView, SIGNAL(clicked(const QModelIndex&)),
+		this, SLOT(itemClicked(const QModelIndex&)));
 }
 
 /**
@@ -788,3 +799,215 @@ void MainWindow::showAbout()
 	
 	aboutDialog->show();
 }
+
+/**
+ * [PRIVATE-SLOT]
+ * Build/active/deactive the menu action based on node selected and its childs
+ */
+void MainWindow::itemClicked(const QModelIndex& index)
+{
+	TreeItem *item = static_cast<TreeItem*>(index.internalPointer());
+
+	//invalid item
+	if(!item || item->isElementProperty())
+	{
+		actionsToolButton->setEnabled(false);
+		return;
+	}
+	
+	//Change node description
+	descriptionLabel->setText(item->getDescrioption());
+	
+	//Enable if item is a property (plugin property)
+	actionsToolButton->setEnabled(item->isProperty());
+	
+	actionMenu->clear();
+	
+	if(!item->isPluginHeaderNode())
+	{
+		QAction *delAction = new QAction(QIcon(":/menu/list_remove"), tr("Remove selected property"), actionMenu);
+		QStringList actionData; 
+		actionData << "delete" << item->getId();
+		delAction->setData(actionData);
+		actionMenu->addAction(delAction);
+		actionMenu->addSeparator();
+	}
+	
+	foreach(ChildDescription cd, item->getChildDescription())
+	{
+		QAction *action = new QAction(QIcon(":/menu/list_add"), tr("Add") + " property: " + cd.name, actionMenu);
+		QStringList actionData;
+		actionData << "add" << item->getId() << cd.id; 
+		
+		action->setData(actionData);
+		actionMenu->addAction(action);
+	}
+}
+
+/**
+ * [PRIVATE-SLOT]
+ * Handle a property action
+ */
+void MainWindow::handlePropertyAction(QAction * action)
+{
+	/* Get infos from action */
+	QStringList actionData = action->data().toStringList();
+	
+	//PLUGIN_NAME{SEPARATOR}PROPERTY_ID{SEPARATOR}PROPERTY_COPY
+	QStringList splittedId = actionData[1].split(SEPARATOR);
+
+	QModelIndex current = propertiesTreeView->currentIndex();
+	QAbstractItemModel *model = propertiesTreeView->model();
+	TreeItem *currentItem = static_cast<TreeItem*>(current.internalPointer());
+	AbstractPropertyHandler *handler = currentItem->getCurrentPropertyHandler();
+	
+	if(!currentItem || !handler)
+		return;
+	
+	//ACTION: delete property
+	if(actionData[0] == "delete")
+	{
+		QString response = handler->removePluginProperty(splittedId[0], splittedId[1], (quint16)splittedId[2].toInt());
+		if(response.isEmpty())
+		{
+			//remove the tree item from tree view
+			bool esite = model->removeRows(current.row(), 1, current.parent());
+			actionsToolButton->setDisabled(esite);
+			
+			if(!esite)
+				qWarning() << "WARNING: property view unconsistence; deleted the plugin property, but not the view item";
+		}
+		else
+		{
+			//Show a warning
+			QMessageBox::warning(this, "Warning - VisualNetkit", response, QMessageBox::Ok);
+		}
+	}
+	
+	//ACTION: add property
+	if(actionData[0] == "add")
+	{
+		QPair<PluginProperty*, QString> response = 
+			handler->addPluginProperty(splittedId[0], actionData[2], splittedId[1],
+					(quint16)splittedId[2].toInt());
+		if(response.second.isEmpty())
+		{
+			//add a new tree item
+			QModelIndex index = propertiesTreeView->selectionModel()->currentIndex();
+			TreeModel *model = static_cast<TreeModel*>(propertiesTreeView->model());
+
+			if(model->insertRows(0, 1, index))
+			{
+				PluginProperty *p = response.first;
+				
+				//Set columns value
+				QModelIndex child0 = model->index(0, 0, index);
+				QModelIndex child1 = model->index(0, 1, index);
+				
+				//This is an hack to prevent a stupid Qt? bug.
+				while(!model->setData(child0, QVariant(p->getName()), Qt::EditRole)) {};
+				while(!model->setData(child1, QVariant(p->getDefaultValue()), Qt::EditRole)) {};
+				
+				if(model->data(child0, Qt::EditRole).toString().isNull())
+				{
+					qDebug() << "OMFG OBSCURE BUG!!!";
+				}
+				
+				//Expand and resize view
+				propertiesTreeView->expandAll();
+				for(int column=0; column < propertiesTreeView->model()->columnCount(); ++column)
+					propertiesTreeView->resizeColumnToContents(column);
+				
+				//Tooltip
+				model->setData(child0, QVariant(p->getDescription()), Qt::ToolTipRole);
+				model->setData(child1, QVariant(p->getDescription()), Qt::ToolTipRole);
+				
+				//Init the low level model (tree item of child0/1)
+				TreeItem *ti = model->getItem(child0);
+				ti->setProperty(true);
+				
+				//PLUGIN_NAME{SEPARATOR}PROPERTY_ID{SEPARATOR}PROPERTY_COPY
+				ti->setId(splittedId[0] + SEPARATOR +
+						p->getId() + SEPARATOR +
+						QString::number(p->getCopy()));
+				
+				PluginProxy *proxy = handler->getPluginFromCurrentElement(splittedId[0]);
+				if(!proxy)
+				{
+					qWarning() << "WARNING: [NULL PROXY] property view unconsistence; added the plugin property, but not the view item";
+					return;
+				}
+				
+				ti->appendChildsDescription(proxy->getPropertyExpert()->getChildsByParentId(p->getId()));
+				
+				ti->setPropertyHandler(handler);
+				
+				ti->setDescription(p->getDescription());
+				
+				buildViewChildsDeeply(child0, p, proxy, handler); //Recursive build
+			}
+			else
+				qWarning() << "Fail creating view row.";
+		}
+		else
+		{
+			//Show a warning
+			QMessageBox::warning(this, "Warning - VisualNetkit", response.second, QMessageBox::Ok);
+		}
+	}
+}
+
+/**
+ * [PRIVATE]
+ * Build view deeply starting from property childs
+ */
+void MainWindow::buildViewChildsDeeply(
+		QModelIndex &child, PluginProperty *prop, PluginProxy *proxy, AbstractPropertyHandler *handler)
+{
+	TreeModel *model = static_cast<TreeModel*>(propertiesTreeView->model());
+	
+	foreach(PluginProperty *p, prop->getChilds())
+	{
+		/* Add a sub-child */
+		if(model->insertRows(0, 1, child))
+		{		
+			//Set columns value
+			QModelIndex child0 = model->index(0, 0, child);
+			QModelIndex child1 = model->index(0, 1, child);
+			
+			//This is an hack to prevent a stupid Qt? bug.
+			while(!model->setData(child0, QVariant(p->getName()), Qt::EditRole)) {};
+			while(!model->setData(child1, QVariant(p->getDefaultValue()), Qt::EditRole)) {};
+			
+			if(model->data(child0, Qt::EditRole).toString().isNull())
+				qDebug() << "--> OMFG OBSCURE BUG!!!";
+			
+			//Expand and resize view
+			propertiesTreeView->expandAll();
+			for(int column=0; column < propertiesTreeView->model()->columnCount(); ++column)
+				propertiesTreeView->resizeColumnToContents(column);
+		
+			
+			//Init the low level model (tree item of child0/1)
+			TreeItem *ti = model->getItem(child0);
+			ti->setProperty(true);
+			
+			//PLUGIN_NAME{SEPARATOR}PROPERTY_ID{SEPARATOR}PROPERTY_COPY
+			ti->setId(proxy->getPlugin()->getName() + SEPARATOR +
+					p->getId() + SEPARATOR +
+					QString::number(p->getCopy()));
+			
+			ti->appendChildsDescription(proxy->getPropertyExpert()->getChildsByParentId(p->getId()));
+			
+			ti->setPropertyHandler(handler);
+			
+			ti->setDescription(p->getDescription());
+			
+			buildViewChildsDeeply(child0, p, proxy, handler);
+		}
+		else
+			qWarning() << "Fail creating view row.";
+	}
+}
+
+
