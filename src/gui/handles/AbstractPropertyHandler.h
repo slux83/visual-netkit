0f4a91e63e1f0769cd50123c6fe97d939a2778e0
slux83 <slux83@70b9100b-7944-0410-84ee-ffaaac3ce8d5>
2008-12-11 20:40:14 +0000

    Little corrections on comments

--- src/gui/handles/AbstractPropertyHandler.h
+++ src/gui/handles/AbstractPropertyHandler.h
@@ -1,17 +1,17 @@
 /**
  * VisualNetkit is an advanced graphical tool for NetKit <http://www.netkit.org>
  * Copyright (C) 2008  Alessio Di Fazio, Paolo Minasi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -31,33 +31,33 @@
  */
 class AbstractPropertyHandler : public QObject
 {
-	
+
 public:
 	/**
 	 * Get a TreeModel that is showed inside the property dock
 	 */
 	virtual TreeModel* getComposedModel() = 0;
-	
+
 	/**
-	 * Get a TreeModl that is showed inside the init plugins property at init time
+	 * Get a TreeModel that is showed inside the init plugins property at init time
 	 */
 	virtual TreeModel* getInitModel(QList<PluginProxy*> plugins) = 0;
-	
+
 	/**
-	 * Save a property (must split treeItem->getId() to get infos
+	 * Save a property (need a treeItem->getId() string split to get informations)
 	 */
 	virtual bool saveChangedProperty(TreeItem *treeItem) = 0;
-	
+
 	/**
 	 * Get from the current `Base Element` the proxy of a plugin
 	 */
 	virtual PluginProxy* getPluginFromCurrentElement(QString pluginName) = 0;
-	
+
 	/**
 	 * Remove a plugin property (consistance control by plugin)
 	 */
 	virtual QString removePluginProperty(QString pluginName, QString propertyUniqueId) = 0;
-	
+
 	/**
 	 * Add a new property (with propertyIdToAdd) of a plugin as child of parentProperty(Id|Copy)
 	 * Return the new PluginProperty* , or NULL on error with error string as second member of
