57c3e1a641f1d851656300d28d3ad3ee645e1d43
slux83 <slux83@70b9100b-7944-0410-84ee-ffaaac3ce8d5>
2008-12-12 21:58:20 +0000

    working on the embedded file manager

---
+++ src/gui/FsTreeView.cpp
@@ -0,0 +1,63 @@
+/**
+ * VisualNetkit is an advanced graphical tool for NetKit <http://www.netkit.org>
+ * Copyright (C) 2008  Alessio Di Fazio, Paolo Minasi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "FsTreeView.h"
+#include <QDebug>
+
+/**
+ * Constructor
+ */
+FsTreeView::FsTreeView(QWidget *parent) : QTreeView(parent)
+{
+	//Get my controller singleton instance
+	fsManager = FsManager::getInstance();
+	contextMenu = new QMenu(this);
+
+	//Create menu actions with connections
+	contextMenu->addAction(QIcon(":/menu/new_icon"), tr("New File"), this, SLOT(newFile()));
+	contextMenu->addAction(QIcon(":/menu/new_folder_icon"), tr("New Folder"), this, SLOT(newFolder()));
+	contextMenu->addSeparator();
+	contextMenu->addAction(QIcon(":/menu/text_editor"), tr("Edit File"), this, SLOT(editFile()));
+	contextMenu->addSeparator();
+	contextMenu->addAction(QIcon(":/menu/trash"), tr("Delete"), this, SLOT(deleteFile()));
+	contextMenu->addAction(QIcon(), tr("Rename..."), this, SLOT(renameFile()));
+	contextMenu->addSeparator();
+	contextMenu->addAction(QIcon(":/menu/refresh"), tr("Refresh"), this, SLOT(refreshView()));
+}
+
+/**
+ * Destructor
+ */
+FsTreeView::~FsTreeView() {}
+
+/**
+ * [PROTECTED]
+ * Context menu action
+ */
+void FsTreeView::contextMenuEvent(QContextMenuEvent *event)
+{
+	QModelIndex mIndex = indexAt(event->pos());		//get the selected item
+
+	qDebug() << "Selected item:" << mIndex.data(QDirModel::FilePathRole);
+
+	if(mIndex.isValid())
+	{
+		current = mIndex;
+		contextMenu->exec(event->globalPos());
+	}
+}
