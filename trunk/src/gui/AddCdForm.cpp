2026376ec4a63a4bb787f5dea04b212d7e63c1f0
slux83 <slux83@70b9100b-7944-0410-84ee-ffaaac3ce8d5>
2009-01-04 11:53:36 +0000

    Fixed plugin dependencies Bug (on load and on selected inside the add vm/cd/link GUI) changed the link gui (the link description)

--- trunk/src/gui/AddCdForm.cpp
+++ trunk/src/gui/AddCdForm.cpp
@@ -1,17 +1,17 @@
 /**
  * VisualNetkit is an advanced graphical tool for NetKit <http://www.netkit.org>
  * Copyright (C) 2008  Alessio Di Fazio, Paolo Minasi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -31,7 +31,7 @@ AddCdForm::AddCdForm(QWidget *parent) : QDialog(parent)
 {
 	/* Connect the UI resource to this QWidget */
 	setupUi(this);
-	
+
 	/* Centering the gui */
 	QDesktopWidget dw;
 	QRect geometry = dw.screenGeometry();
@@ -39,19 +39,19 @@ AddCdForm::AddCdForm(QWidget *parent) : QDialog(parent)
 				(geometry.height() / 2) - (height() / 2),	//center Y
 				width(),
 				height());
-	
+
 	cdHandler = CdHandler::getInstance();
-	
+
 	/* Connects */
 	connect(cdFormConfirmBox, SIGNAL(accepted()),
 			this, SLOT(handleUserConfirm()));
-	
+
 	connect(this, SIGNAL(userAddCd(QString, QStringList, bool, QPointF)),
-				cdHandler, SLOT(handleAddNewCd(QString, QStringList, bool, QPointF)));	
-	
+				cdHandler, SLOT(handleAddNewCd(QString, QStringList, bool, QPointF)));
+
 	connect(pluginsList, SIGNAL(itemClicked(QListWidgetItem *)),
 					this, SLOT(showPluginInfos(QListWidgetItem *)));
-	
+
 	/* Init plugin chooser */
 	availablePlugins = PluginRegistry::getInstance()->getAllPluginFactories();
 	fillPluginChooser();
@@ -75,7 +75,7 @@ void AddCdForm::handleUserConfirm()
 	bool ok = true;
 	QRegExpValidator nameValidator(QRegExp("^[a-zA-Z0-9]+$"), this);
 	int pos = 0;
-	
+
 	/* validate name */
 	if(nameValidator.validate(cdName, pos) != QValidator::Acceptable)
 	{
@@ -83,35 +83,35 @@ void AddCdForm::handleUserConfirm()
 		QMessageBox::warning(this, tr("VisualNetkit - Error"),
 				tr("The collision domain name must match ^[a-zA-Z0-9]+$"),
 				QMessageBox::Ok);
-		
+
 		ok = false;
 	}
-	
+
 	if(ok && CdHandler::getInstance()->cdNameExist(cdName))
 	{
 		/* Show a warning message */
 		QMessageBox::warning(this, tr("VisualNetkit - Error"),
 				tr("The collision domain name must be unique!\nPlease, retry."),
 				QMessageBox::Ok);
-		
+
 		ok = false;
 	}
-	
-	
+
+
 	/**
 	 * ok, all seems correct.. emit a signal, clear gui and close
 	 */
 	if(ok)
 	{
 		QStringList selPlugins = getSelectedPlugins();
-		
+
 		emit userAddCd(cdName, selPlugins,
 				(initPropertiesCheck->checkState() == Qt::Unchecked), cdPos);
-		
+
 		cdNameLineEdit->clear();
 		close();
 	}
-	
+
 }
 
 /**
@@ -124,18 +124,18 @@ void AddCdForm::fillPluginChooser()
 	while(it.hasNext())
 	{
 		PluginLoaderFactory *factory = it.next();
-		
+
 		//it's a mine plugin?
 		if(factory->getType() != "cd")
 			continue; //Skeep this plugin
-		
+
 		//create entry
 		QListWidgetItem *pluginItem = new QListWidgetItem();
 		pluginItem->setIcon(QIcon(QString::fromUtf8(":/small/plugin")));
 		pluginItem->setData(Qt::DisplayRole, factory->getName());	//it's the unique ID
 		pluginItem->setData(Qt::ToolTipRole, tr("Select this plugin to show extra infos."));
 		pluginItem->setCheckState(Qt::Unchecked);
-		
+
 		pluginsList->addItem(pluginItem);
 	}
 }
@@ -151,7 +151,7 @@ void AddCdForm::showPluginInfos(QListWidgetItem *item)
 	while(it.hasNext())
 	{
 		PluginLoaderFactory *factory = it.next();
-		
+
 		if(factory->getName() == selectedPluginName)
 		{
 			/* Render infos */
@@ -160,7 +160,28 @@ void AddCdForm::showPluginInfos(QListWidgetItem *item)
 			pDeps->setText(factory->getDeps().join(", "));
 			pAuthor->setText(factory->getAuthor());
 			pVersion->setText(factory->getVersion());
-			
+
+			/* control dependencies if it's checked */
+			if(item->checkState() == Qt::Checked)
+			{
+				QStringList notFoundDeps;
+				//select all deps
+				foreach(QString dep, factory->getDeps())
+				{
+					QList<QListWidgetItem *> listItems =
+						pluginsList->findItems(dep, Qt::MatchExactly);
+					if(listItems.size() == 0)
+						notFoundDeps << dep;	//save the not founded dep
+					else
+						listItems.first()->setCheckState(Qt::Checked);		//check the dep
+				}
+
+				//we have not found dependencies??
+				if(!notFoundDeps.isEmpty())
+					QMessageBox::warning(this, "Warning",
+							tr("There are dependency problems") + "\nPlugins (" + notFoundDeps.join(", ") + ") not found.");
+			}
+
 			break;
 		}
 	}
@@ -173,11 +194,11 @@ void AddCdForm::showPluginInfos(QListWidgetItem *item)
 QStringList AddCdForm::getSelectedPlugins()
 {
 	QStringList selectedPlugins;
-	
+
 	/* Get all list items and select only selected */
 	QList<QListWidgetItem *> listItems =
 		pluginsList->findItems(".+", Qt::MatchRegExp);
-	
+
 	QListIterator<QListWidgetItem *> itemIter(listItems);
 	while(itemIter.hasNext())
 	{
@@ -186,7 +207,7 @@ QStringList AddCdForm::getSelectedPlugins()
 		if(item->checkState() == Qt::Checked)
 			selectedPlugins.append(item->data(Qt::DisplayRole).toString());
 	}
-	
+
 	return selectedPlugins;
 }
 
--- trunk/src/gui/AddLinkForm.cpp
+++ trunk/src/gui/AddLinkForm.cpp
@@ -1,17 +1,17 @@
 /**
  * VisualNetkit is an advanced graphical tool for NetKit <http://www.netkit.org>
  * Copyright (C) 2008  Alessio Di Fazio, Paolo Minasi
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -29,7 +29,7 @@ AddLinkForm::AddLinkForm(QWidget *parent) : QDialog(parent)
 {
 	/* Connect the UI resource to this QDialog */
 	setupUi(this);
-	
+
 	/* Centering the gui */
 	QDesktopWidget dw;
 	QRect geometry = dw.screenGeometry();
@@ -38,24 +38,24 @@ AddLinkForm::AddLinkForm(QWidget *parent) : QDialog(parent)
 				width(),
 				height());
 
-	
+
 	cdItem = NULL;
 	vmItem = NULL;
-	
+
 	interfacesTable->horizontalHeader()->setResizeMode(QHeaderView::Stretch);
-	
+
 	/* connects */
 	connect(addLinkButtonBox, SIGNAL(accepted()),
 			this, SLOT(handleAccept()));
-	
+
 	connect(this,
 			SIGNAL(readyToAddLink(VirtualMachineItem *, CollisionDomainItem *, QStringList, bool, QString, bool)),
 			LinkHandler::getInstance(),
 			SLOT(createLink(VirtualMachineItem *, CollisionDomainItem *, QStringList, bool, QString, bool)));
-	
+
 	connect(pluginsList, SIGNAL(itemClicked(QListWidgetItem *)),
 				this, SLOT(showPluginInfos(QListWidgetItem *)));
-	
+
 	/* Init plugin chooser */
 	availablePlugins = PluginRegistry::getInstance()->getAllPluginFactories();
 	fillPluginChooser();
@@ -75,27 +75,27 @@ void AddLinkForm::updateItems(VirtualMachineItem *vm, CollisionDomainItem* cd)
 {
 	vmItem = vm;
 	cdItem = cd;
-	
+
 	//just a test
 	if(vmItem == NULL || cdItem == NULL)
 	{
 		qWarning() << "AddLinkForm: vmItem or cdItem is null";
 		return;
 	}
-	
+
 	//update gui
 	labelVm->setText(vm->getLabel());
 	labelCd->setText(cd->getLabel());
-	
+
 	/* Get interfaces for the vm */
 	QStringListIterator iter(VmMapper::getInstance()->getMachineInterfaces(vm));
-	
-	
+
+
 	/* Render interfaces inside the table [NAME] */
 	int row = 0;
 	//Clear all table items and reset the view-size
 	interfacesTable->clearContents();
-	interfacesTable->setRowCount(0);		//resize (reset) the view	
+	interfacesTable->setRowCount(0);		//resize (reset) the view
 	while(iter.hasNext())
 	{
 		interfacesTable->setRowCount(interfacesTable->rowCount() + 1);
@@ -111,7 +111,7 @@ void AddLinkForm::updateItems(VirtualMachineItem *vm, CollisionDomainItem* cd)
  * Handle accept()
  */
 void AddLinkForm::handleAccept()
-{	
+{
 	/**
 	 * test the interface name
 	 */
@@ -121,21 +121,21 @@ void AddLinkForm::handleAccept()
 		QMessageBox::warning(this, tr("VisualNetkit - Error"),
 			tr("The interface name must be unique!\nPlease, retry."),
 			QMessageBox::Ok);
-		
+
 		return;
 	}
-	
+
 	//if up --> state true, false otherwise
 	bool ethState = ethUp->isChecked();
-	
+
 	/* Ok, get active plugins and foward the request */
 	QStringList selPlugins = getSelectedPlugins();
-	
+
 	//emit signal and close the gui
 	emit readyToAddLink(vmItem, cdItem, selPlugins, (initPropertiesCheck->checkState() == Qt::Unchecked),
 			QString(ethNumberSpinBox->prefix() + QString::number(ethNumberSpinBox->value())),
 			ethState);
-	
+
 	close();
 }
 
@@ -149,18 +149,18 @@ void AddLinkForm::fillPluginChooser()
 	while(it.hasNext())
 	{
 		PluginLoaderFactory *factory = it.next();
-		
+
 		//it's a mine plugin?
 		if(factory->getType() != "link")
 			continue; //Skeep this plugin
-		
+
 		//create entry
 		QListWidgetItem *pluginItem = new QListWidgetItem();
 		pluginItem->setIcon(QIcon(QString::fromUtf8(":/small/plugin")));
 		pluginItem->setData(Qt::DisplayRole, factory->getName());	//it's the unique ID
 		pluginItem->setData(Qt::ToolTipRole, tr("Select this plugin to show extra infos."));
 		pluginItem->setCheckState(Qt::Unchecked);
-		
+
 		pluginsList->addItem(pluginItem);
 	}
 }
@@ -176,7 +176,7 @@ void AddLinkForm::showPluginInfos(QListWidgetItem *item)
 	while(it.hasNext())
 	{
 		PluginLoaderFactory *factory = it.next();
-		
+
 		if(factory->getName() == selectedPluginName)
 		{
 			/* Render infos */
@@ -185,7 +185,28 @@ void AddLinkForm::showPluginInfos(QListWidgetItem *item)
 			pDeps->setText(factory->getDeps().join(", "));
 			pAuthor->setText(factory->getAuthor());
 			pVersion->setText(factory->getVersion());
-			
+
+			/* control dependencies if it's checked */
+			if(item->checkState() == Qt::Checked)
+			{
+				QStringList notFoundDeps;
+				//select all deps
+				foreach(QString dep, factory->getDeps())
+				{
+					QList<QListWidgetItem *> listItems =
+						pluginsList->findItems(dep, Qt::MatchExactly);
+					if(listItems.size() == 0)
+						notFoundDeps << dep;	//save the not founded dep
+					else
+						listItems.first()->setCheckState(Qt::Checked);		//check the dep
+				}
+
+				//we have not found dependencies??
+				if(!notFoundDeps.isEmpty())
+					QMessageBox::warning(this, "Warning",
+							tr("There are dependency problems") + "\nPlugins (" + notFoundDeps.join(", ") + ") not found.");
+			}
+
 			break;
 		}
 	}
@@ -198,11 +219,11 @@ void AddLinkForm::showPluginInfos(QListWidgetItem *item)
 QStringList AddLinkForm::getSelectedPlugins()
 {
 	QStringList selectedPlugins;
-	
+
 	/* Get all list items and select only selected */
 	QList<QListWidgetItem *> listItems =
 		pluginsList->findItems(".+", Qt::MatchRegExp);
-	
+
 	QListIterator<QListWidgetItem *> itemIter(listItems);
 	while(itemIter.hasNext())
 	{
@@ -211,6 +232,6 @@ QStringList AddLinkForm::getSelectedPlugins()
 		if(item->checkState() == Qt::Checked)
 			selectedPlugins.append(item->data(Qt::DisplayRole).toString());
 	}
-	
+
 	return selectedPlugins;
 }
